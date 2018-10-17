0a4b38d46cade0572efc603bd0cef44b3d2837ff
Thad <Thadip@icloud.com>
2018-10-16 20:39:23 -0500

    First Commit

---
+++ 16.5_ThadSwint/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace _16._5_ThadSwint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TBox = new System.Windows.Forms.TextBox();
+            this.TranslatedLabel = new System.Windows.Forms.Label();
+            this.translateButton = new System.Windows.Forms.Button();
+            this.CountLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TBox
+            // 
+            this.TBox.Location = new System.Drawing.Point(13, 13);
+            this.TBox.MaxLength = 300;
+            this.TBox.Multiline = true;
+            this.TBox.Name = "TBox";
+            this.TBox.Size = new System.Drawing.Size(180, 54);
+            this.TBox.TabIndex = 0;
+            // 
+            // TranslatedLabel
+            // 
+            this.TranslatedLabel.AutoSize = true;
+            this.TranslatedLabel.Location = new System.Drawing.Point(12, 97);
+            this.TranslatedLabel.MaximumSize = new System.Drawing.Size(378, 2);
+            this.TranslatedLabel.Name = "TranslatedLabel";
+            this.TranslatedLabel.Size = new System.Drawing.Size(0, 2);
+            this.TranslatedLabel.TabIndex = 1;
+            // 
+            // translateButton
+            // 
+            this.translateButton.Location = new System.Drawing.Point(372, 304);
+            this.translateButton.Name = "translateButton";
+            this.translateButton.Size = new System.Drawing.Size(97, 23);
+            this.translateButton.TabIndex = 2;
+            this.translateButton.Text = "Translate";
+            this.translateButton.UseVisualStyleBackColor = true;
+            this.translateButton.Click += new System.EventHandler(this.TranslateButton_Click);
+            // 
+            // CountLabel
+            // 
+            this.CountLabel.AutoSize = true;
+            this.CountLabel.Location = new System.Drawing.Point(24, 304);
+            this.CountLabel.Name = "CountLabel";
+            this.CountLabel.Size = new System.Drawing.Size(0, 17);
+            this.CountLabel.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 339);
+            this.Controls.Add(this.CountLabel);
+            this.Controls.Add(this.translateButton);
+            this.Controls.Add(this.TranslatedLabel);
+            this.Controls.Add(this.TBox);
+            this.Name = "Form1";
+            this.Text = "Pig Latin Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TBox;
+        private System.Windows.Forms.Label TranslatedLabel;
+        private System.Windows.Forms.Button translateButton;
+        private System.Windows.Forms.Label CountLabel;
+    }
+}
+
---
+++ 16.5_ThadSwint/Form1.cs
@@ -0,0 +1,84 @@
+﻿// Thad Swint Simple Pig Latin Converter
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _16._5_ThadSwint
+{
+    public partial class Form1 : Form
+    {
+        private string aY = "ay";
+        private string textTaken;
+        private int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TranslateButton_Click(object sender, EventArgs e)
+        {
+            GetPigLatin();
+
+        }
+
+        private void GetPigLatin()
+        {
+            // Prevent anything less than 2 chars -- alert if true
+            if (TBox.Text.Length < 2)
+            {
+                MessageBox.Show("you must have at least 2 characters");
+                return;
+            }
+            // Prevent non-alphabetical input
+            if (!(System.Text.RegularExpressions.Regex.IsMatch(TBox.Text, "^[a-zA-Z ]*$")))
+            {
+                MessageBox.Show("Only alphabetical input please & no returns");
+                return;
+            }
+
+            // Declare local variales 
+            textTaken = TBox.Text;
+            TranslatedLabel.MaximumSize = new Size(278, 0);
+            TranslatedLabel.AutoSize = true;
+            StringBuilder buffer = new StringBuilder();
+            string[] words = textTaken.Split(' ');
+
+            // Iterate through each word 
+                foreach (var word in words)
+                {
+                try
+                {
+                    // Grab the first letter & concat ay
+                    string firstLetterWithAy = word[0].ToString() + aY;
+                    // Grab the word minus the first letter
+                    string wordWithoutFirstLetter = word.Remove(0, 1);
+                    // Create the pig latin word
+                    string latin = wordWithoutFirstLetter + firstLetterWithAy;
+                    // Append the pig latin word to string builder var with a space
+                    buffer.Append(latin + " ");
+                }
+                // Caused by unwanted spaces 
+                catch (IndexOutOfRangeException)
+                {
+                    MessageBox.Show("Too many spaces: \nOnly one allowed\nMust not begin with a space" +
+                        "\nMust not end with a space ");
+                    return;
+                }
+                }
+                /* 
+                 * If we made it this far output the buffer to the label
+                 * Add to the counter
+                 * Have our label reflect the count 
+                 */
+            TranslatedLabel.Text = buffer.ToString().ToUpper();
+            count++;
+            CountLabel.Text = ("Successful translation = " + count.ToString());
+        }
+    }
+}
